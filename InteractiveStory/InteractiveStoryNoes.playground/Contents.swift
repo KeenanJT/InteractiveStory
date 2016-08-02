022427dbfb2596ce1d74635028a547ae856df919
KeenanJT <keenturner@gmail.com>
2016-08-02 12:24:40 -0400

    Launch Adventure Screen Implemented

---
+++ InteractiveStory/InteractiveStoryNoes.playground/Contents.swift
@@ -0,0 +1,28 @@
+////////////////////////////
+// Navigation Controllers //
+////////////////////////////
+
+/*
+ Navigation Controller - 
+ 1) "Content View" To present your custom views which are contained inside individual view controllers
+ 2) "Navigation Bar" To display a few custom views on its own. These views are navigation bars: back button, label, or others.
+ 3) Adds or removes View Controllers
+ 4) Stores an array of View Controllers
+ 
+ View Controller -
+ 1)Manages the view; shows, animates, hides, or do whatever it needs
+ */
+
+///////////////////////////
+// Root View Controllers //
+///////////////////////////
+
+/*
+ Main window of an app; base content view
+ 
+ first arrow -> makes a storyboard entry point. This makes the Root View
+ 
+ When setting the Navigation Controller as the Root View, there will be a new View Controller created to be the Navigation Controller's initial view.
+ 
+ */
+
